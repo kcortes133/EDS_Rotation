54a56ef6df11593ed1776dd17bae725bef6c8d9f
Katherina <kcortes@ucsd.edu>
2022-03-30 10:26:37 -0600

    create repo

---
+++ main.py
@@ -0,0 +1,6 @@
+# Author: Katherina Cortes
+# Date: March 24, 2022
+# Purpose: rotation intro
+
+# install jupyter ntbk
+# https://github.com/NCATS-Tangerine/cq-notebooks/tree/master/Orange_Demonstrator_1_CQs/OrangeQ1.1_PPI_Network
